bdd6f15d7b7d76be9a91af9ea3c540de7dbce30d
John Doe <randomEme@protonmail.com>
2020-01-08 15:03:11 +0400

    Картинки теперь собираются из конфига и json

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,6 +1,12 @@
 <template>
     <li class="movie-list__item">
+        <h2>{{movie.title}}</h2>
+        <p>Release date: {{movie.release_date}}</p>
+        <img v-bind:src=image>
         {{movie}}
+        {{config}}
+        {{config.secure_base_url + config.backdrop_sizes[0] + movie.poster_path}}
+        <img v-bind:src="require(`${config.secure_base_url}${config.backdrop_sizes[0]}${movie.poster_path}`)"
     </li>
 </template>
 
@@ -10,6 +16,15 @@ export default {
         movie: {
             type: Object,
             required: true
+        },
+        config: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            image: this.config.secure_base_url + this.config.backdrop_sizes[0] + this.movie.poster_path
         }
     }
 }
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -4,6 +4,7 @@
             v-for="movie in movies"
             v-bind:key="movie.id"
             v-bind:movie="movie"
+            v-bind:config="config"
         />
     </ul>
 </template>
@@ -13,8 +14,22 @@ import MovieItem from "@/components/MovieItem";
 
 export default {
     props: ["movies"],
+    data() {
+        return {
+            config: {}
+        }
+    },
     components: {
         MovieItem
-    }
+    },
+    async mounted() {
+        let images = await fetch("https://api.themoviedb.org/3/configuration?api_key=60e1831dec35a216fdaff508cdf5675c");
+        let paths =  await images.json();
+        // console.log(paths.images);
+        // this.image = paths.images.secure_base_url + paths.images.backdrop_sizes[0] + this.movie.poster_path;
+        this.config = paths.images;
+        console.log(this.config);
+    // this.image = paths.images;
+  }
 }
 </script>
